6f52f903bf89d4cc1b3bf5047238e5bb86134dc4
prathamesh patil <prathm.patil.100@gmail.com>
2021-12-16 14:52:04 +0530

    Project neko

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,32 @@
-console.log("javascript loaded")
+const button = document.getElementById('button');
+const button_fact = document.getElementById('button-fact');
+
+button.addEventListener('click',getCatImage);
+button_fact.addEventListener('click',getCatFact);
+
+function getCatImage(){
+    fetch('https://cataas.com/cat?json=true')
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data);
+        
+        document.getElementById('out-image').innerHTML = `<img src="https://cataas.com${data.url}" class="cat-image">`;
+    })
+}
+
+function getCatFact(){
+    fetch('https://cat-fact.herokuapp.com/facts/random?amount=3')
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data);
+        let out = '';
+        for(i in data){
+            out += `<div class="fact">
+                <p>${data[i].text}</p>
+            </div>`
+        }
+        document.getElementById('out').innerHTML = out;
+    })
+}
+getCatFact();
+getCatImage();
